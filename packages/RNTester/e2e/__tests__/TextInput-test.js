8a56ce0905bc84d11879e8dfcd1080dfa8408b0e
agarwalmanya <manya18ag@gmail.com>
2020-07-21 15:26:31 +0530

    tests: change description based on convention

--- packages/RNTester/e2e/__tests__/TextInput-test.js
+++ packages/RNTester/e2e/__tests__/TextInput-test.js
@@ -23,14 +23,14 @@ describe('TextInput', () => {
     );
   });
 
-  it('TextInput should accept input', async () => { 
+  it('should accept input', async () => { 
     await openExampleWithTitle("TextInput with autoFocus={true}"); 
 
     await element(by.id('check_text_input')).typeText("Check text input"); 
     await expect(element(by.id('check_text_input'))).toHaveText("Check text input"); 
   })
 
-  it('No-spaces-Input should not allow spaces', async () => {
+  it('should not allow spaces in no-spaces-input', async () => {
     await openExampleWithTitle("TextInput that replaces spaces with underscore '_'");
 
     await element(by.id('rewrite_sp_underscore_input')).typeText(
@@ -40,7 +40,7 @@ describe('TextInput', () => {
     await expect(element(by.id('rewrite_sp_underscore_input'))).toHaveText('this_is_a_long_sentence');
   });
 
-  it('TextInput with maxLength should not exceed maximum length', async () => {
+  it('should not exceed maximum length when maxLength is used', async () => {
     await openExampleWithTitle("TextInput with maxLength={limit}"); 
 
     await element(by.id('max_length_input')).typeText(
@@ -50,11 +50,10 @@ describe('TextInput', () => {
     await expect(element(by.id('max_length_input'))).toHaveText('this is a very long ');
   })
 
-  it("TextInput should be cleared by tapping 'Clear' button", async () => { 
+  it("should be cleared by tapping 'Clear' button", async () => { 
     await openExampleWithTitle("TextInput along with 'Clear' button"); 
     await element(by.id('clear_text_input')).typeText('this text needs to be cleared'); 
     await element(by.id('rewrite_clear_button')).tap(); 
-
     await expect(element(by.id('clear_text_input'))).toHaveText('');
   })
 });
