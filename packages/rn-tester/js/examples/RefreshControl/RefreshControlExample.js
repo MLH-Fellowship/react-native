fce966efa37aeecc7fa4451e3f48c47bec021d44
chirag-singhal <csinghal208@gmail.com>
2020-08-18 12:21:58 +0530

    add documentationURL to example screens

--- packages/rn-tester/js/examples/RefreshControl/RefreshControlExample.js
+++ packages/rn-tester/js/examples/RefreshControl/RefreshControlExample.js
@@ -115,6 +115,7 @@ class RefreshControlExample extends React.Component {
 }
 
 exports.title = 'RefreshControl';
+exports.documentationURL = 'https://reactnative.dev/docs/refreshcontrol';
 exports.description = 'Adds pull-to-refresh support to a scrollview.';
 exports.simpleExampleContainer = true;
 exports.examples = [
--- packages/rn-tester/js/examples/Touchable/TouchableExample.js
+++ packages/rn-tester/js/examples/Touchable/TouchableExample.js
@@ -554,6 +554,7 @@ const styles = StyleSheet.create({
 exports.displayName = (undefined: ?string);
 exports.description = 'Touchable and onPress examples.';
 exports.title = 'Touchable* and onPress';
+exports.documentationURL = 'https://reactnative.dev/docs/touchablehighlight';
 exports.examples = [
   {
     title: '<TouchableHighlight>',
