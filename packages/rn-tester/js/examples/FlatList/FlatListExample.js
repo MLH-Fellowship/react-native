026bad20b26a45477a31d7c0c6d504a33a5d804e
CHIRAG SINGHAL <csinghal208@gmail.com>
2020-08-18 18:10:44 +0530

    Add documentation URL to examples and remove community screens (#224)
    * add documentationURL to example screens
    
    * remove community package screens
    
    * delete masked view iOS example

--- packages/rn-tester/js/examples/FlatList/FlatListExample.js
+++ packages/rn-tester/js/examples/FlatList/FlatListExample.js
@@ -273,6 +273,7 @@ const styles = StyleSheet.create({
 });
 
 exports.title = 'FlatList';
+exports.documentationURL = 'https://reactnative.dev/docs/flatlist';
 exports.category = 'ListViews';
 exports.description = 'Performant, scrollable list of data.';
 exports.simpleExampleContainer = true;
