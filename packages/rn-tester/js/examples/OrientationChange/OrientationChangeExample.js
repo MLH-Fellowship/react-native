468762f1c7cd2e7befc1be9790d8cf44391fe4eb
Ansh Godha <ag759@cornell.edu>
2020-08-20 13:37:37 -0400

    Add orientation screen rewrite

--- packages/rn-tester/js/examples/OrientationChange/OrientationChangeExample.js
+++ packages/rn-tester/js/examples/OrientationChange/OrientationChangeExample.js
@@ -14,13 +14,13 @@ const React = require('react');
 
 const {DeviceEventEmitter, Text, View} = require('react-native');
 
-import {type EventSubscription} from '../../../../Libraries/vendor/emitter/EventEmitter';
+import type EmitterSubscription from '../../../../Libraries/vendor/emitter/EmitterSubscription';
 
 class OrientationChangeExample extends React.Component<{...}, $FlowFixMeState> {
-  _orientationSubscription: EventSubscription;
+  _orientationSubscription: EmitterSubscription;
 
   state = {
-    currentOrientation: '',
+    currentOrientation: 'potrait-primary',
     orientationDegrees: 0,
     isLandscape: false,
   };
@@ -46,15 +46,21 @@ class OrientationChangeExample extends React.Component<{...}, $FlowFixMeState> {
 
   render() {
     return (
-      <View>
-        <Text>{JSON.stringify(this.state)}</Text>
+      <View style={{alignItems: 'center'}}>
+        <Text style={{textAlign: 'center', width: '80%', marginVertical: 20}}>
+          Hello there, React Native user! Your device is currently on{' '}
+          {this.state.currentOrientation.endsWith('primary')
+            ? 'primary'
+            : 'secondary'}{' '}
+          {this.state.isLandscape ? 'landscape' : 'potrait'} mode. This is at an
+          angle of {this.state.orientationDegrees} from its default position.
+        </Text>
       </View>
     );
   }
 }
 
 exports.title = 'OrientationChangeExample';
-exports.category = 'Basic';
 exports.description = 'listening to orientation changes';
 exports.examples = [
   {
